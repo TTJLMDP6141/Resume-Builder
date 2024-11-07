07200c5bf0d446e2e8928dd1ef212f536b233611
TTJLMDP6141 <131487957+TTJLMDP6141@users.noreply.github.com>
2024-11-07 13:39:19 +0530

    Update dbConnect.js

--- dbConnect.js
+++ dbConnect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URL = 'mongodb+srv://tusharjadhav2515:tushar@cluster0.s52ue7m.mongodb.net/Resume-Builder';
+const URL = 'mongodb+srv://tusharjadhav:tu1111@cluster0.s52ue7m.mongodb.net/Resume';
 mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
 const connection = mongoose.connection
 connection.on('connected', () => {
